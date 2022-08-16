505e1b0185c35c356ea6864722851c6737205889
Eugene <eugene.multan@gmail.com>
2022-08-16 10:16:18 +0300

    remove redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { contacts } from './reducers';
 
-import { persistStore } from 'redux-persist';
-
 const store = configureStore({
   reducer: {
     contacts,
   },
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST'],
-      },
-    }),
-  ],
 });
-
-export let persistor = persistStore(store);
 
 export default store;
